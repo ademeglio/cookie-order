693d16945d861af06d7d3c8f856eb1401f5e8690
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-05-27 15:21:36 -0400

    added a cancel to varietyMenu(); complete OrderApp;

--- src/main/java/org/wecancodeit/cookie_order/OrderApp.java
+++ src/main/java/org/wecancodeit/cookie_order/OrderApp.java
@@ -3,44 +3,46 @@ package org.wecancodeit.cookie_order;
 import java.util.Scanner;
 
 public class OrderApp {
-
+	
 	public static void main(String[] args) {
 		
 		boolean isOrderComplete = false;
-		String yesNo;
-		String variety;
+		String yesNo = "";
+		String variety = "";
 		
 		Scanner userInput = new Scanner(System.in);
 		MasterOrder userOrder = new MasterOrder();
 		
 // Add order(s) of cookies.
-		while (!isOrderComplete) {
+		while (isOrderComplete == false) {
 			System.out.println("Please select one variety of cookie to purchase:");
 			variety = varietyMenu(userInput); // displays menu of available variety and asks for selection.
 			
 			System.out.println("How many boxes of " + variety + " would you like to buy?");
 			int numBoxes = userInput.nextInt(); // enter quantity desired of selected variety
 			
-			userOrder.addOrder(new CookieOrder(variety, numBoxes));
+			userOrder.addOrder(new CookieOrder(variety, numBoxes)); // create new Cookie Order
+			System.out.println("There are [" + userOrder.getTotalBoxes() + "] box[es] in your cart.\n"); // Total boxes in the cart
+			userInput.nextLine();
 			
+			// continue the loop?
 			System.out.println("Would you like to buy more? Type y or n");
-			yesNo = userInput.nextLine();
-			if (yesNo.toLowerCase() == "n" || yesNo.toLowerCase() == "no") {
+			yesNo = userInput.nextLine(); // shops the scanner which doesn't close because .nextInt()
+			
+			if (yesNo.equals("n") || yesNo.equals("no")) {
 				isOrderComplete = true;
-			} else {
-				continue;
-			}
+				break;
+			} 
 		}
-// Total the boxes purchased
-		System.out.println("There are [" + userOrder.getTotalBoxes() + "] box[es] in your cart.\n");
-		
+	
 // Show the list
+		System.out.println("Your shopping cart currently has:\n ");
 		userOrder.showOrder();
 		
 //	Remove a variety and give feedback on how many boxes were removed
-		System.out.println("Would you like to remove any selections from your car?");
+		System.out.println("\nWould you like to remove any selections from your car? Type y or n");
 		yesNo = userInput.next();
-		if (yesNo.toLowerCase() == "y" || yesNo.toLowerCase() == "yes") {
+		if (yesNo.toLowerCase().equals("y") || yesNo.toLowerCase().equals("yes")) {
 			System.out.println("Please select the variety to remove...");
 			variety = varietyMenu(userInput);
 			userOrder.removeVariety(variety);
@@ -53,8 +55,8 @@ public class OrderApp {
 	public static String varietyMenu(Scanner userInput) {
 		while (true) {
 			
-			System.out.println("1. Tagalongs     3. Thin Mints     5. S'mores"
-						   + "\n2. Samoas        4. Shortbread     6. EXIT");
+			System.out.println("1. Tagalongs     3. Thin Mints     5. S'mores    7. EXIT"
+						   + "\n2. Samoas        4. Shortbread     6. CANCEL");
 			
 			int choice = userInput.nextInt();
 			switch (choice) {
@@ -68,9 +70,11 @@ public class OrderApp {
 				return "Shortbreak";
 			case 5:
 				return "S'mores";
-			case 6:
+			case 6: //cancel
+				return null;
+			case 7:
 				System.exit(0);
-			Default:
+			default:
 				System.out.println("Please make a valid selection");	
 			}
 		}
